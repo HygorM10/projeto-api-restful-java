ffa5aa31610c638a9ba86e5e4adc307fec1bbfc5
Hygor Martins <hygor.martins74@gmail.com>
2021-08-15 20:00:33 -0300

    Estudando BCrypt para tratamento de senhas na base de dados

---
+++ src/main/java/com/hygor/api/ProjetoApiRestfulCursoApplication.java
@@ -0,0 +1,13 @@
+package com.hygor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiRestfulCursoApplication {
+
+	public static void main(String[] args) {
+ 		SpringApplication.run(ProjetoApiRestfulCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hygor/api/ProjetoApiRestfulCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hygor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiRestfulCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
